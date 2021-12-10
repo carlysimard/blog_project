a331e609cc20089fdf8e8fc087e21f67ec31447e
carlysimard <carly.simard@snhu.edu>
2021-12-10 13:47:03 -0500

    initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,18 @@
-#!/usr/bin/env python
-__author__ = "Carly Simard"
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import (
+    BlogListView,
+    BlogUpdateView,
+    BlogDetailView,
+    BlogCreateView,
+    BlogDeleteView,
+)
+
 urlpatterns = [
-    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # new
+    path('post/<int:pk>/delete/',  # new
+        BlogDeleteView.as_view(), name='post_delete'),
+    path('post/new/', BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
+    path('post/<int:pk>/edit/',
+        BlogUpdateView.as_view(), name='post_edit'),
     path('', BlogListView.as_view(), name='home'),
 ]
